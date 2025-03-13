689f17dfe75b9ef5cf67cdd36b0ee20d7455f3dc
azacdev <azacdev@gmail.com>
2025-03-14 00:52:10 +0100

    feat: implement initial API structure and load environment variables

---
+++ api/controllers/entries.go
@@ -0,0 +1,31 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func CreateEntry(c *gin.Context) {
+
+}
+
+func GetEntreies(c *gin.Context) {
+
+}
+
+func GetEntry(c *gin.Context) {
+
+}
+
+func GetEntryByIngredient(c *gin.Context) {
+
+}
+
+func UpdateEntry(c *gin.Context) {
+
+}
+
+func UpdateIngredient(c *gin.Context) {
+
+}
+
+func DeleteEntry(c *gin.Context) {
+
+}
--- main.go
+++ main.go
@@ -3,14 +3,35 @@ package main
 import (
 	"fmt"
 
+	"github.com/azacdev/go-react/api/controllers"
+	"github.com/azacdev/go-react/config"
 	"github.com/azacdev/go-react/database"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func init() {
+	config.LoadEnvVariables()
 	database.ConnectToDB()
 	database.SyncDatabase()
 }
 
 func main() {
+	r := gin.Default()
+
+	r.Use(gin.Logger())
+	r.Use(cors.Default())
 	fmt.Println("Wubba lubba dub dub")
+
+	r.GET("/entries", controllers.GetEntreies)
+	r.GET("/entry/:id", controllers.GetEntry)
+	r.GET("/ingredient/:ingredient", controllers.GetEntryByIngredient)
+
+	r.POST("/entry/create", controllers.CreateEntry)
+
+	r.PUT("/entry/update/:id", controllers.UpdateEntry)
+	r.PUT("/ingredient/update/:id", controllers.UpdateIngredient)
+	r.DELETE("/entry/delete/:id", controllers.DeleteEntry)
+
+	r.Run()
 }
